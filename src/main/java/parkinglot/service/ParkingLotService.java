80a2822f4b234b7de7e3745dd3f451d1e1d6b151
Srijan-18 <srijan.mishrasir@gmail.com>
2020-07-18 05:15:53 +0530

    ADDED: Code to park the car and check for its parking status.

---
+++ src/main/java/parkinglot/service/ParkingLotService.java
@@ -0,0 +1,21 @@
+package parkinglot.service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ParkingLotService {
+
+    private Map<String, String> parkedCars;
+
+    public ParkingLotService() {
+        parkedCars = new HashMap<>();
+    }
+
+    public void parkTheCar(String carNumber) {
+        parkedCars.put(carNumber, carNumber);
+    }
+
+    public boolean isCarPresent(String carNumber) {
+        return parkedCars.containsKey(carNumber);
+    }
+}
