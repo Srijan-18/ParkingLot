4d344c6313b09f42a92446890f012852083b2b4c
Srijan-18 <srijan.mishrasir@gmail.com>
2020-07-18 05:32:56 +0530

    ADDED: Test case to validate driver unparks the car.

--- src/test/java/ParkingLotTest.java
+++ src/test/java/ParkingLotTest.java
@@ -1,14 +1,31 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import parkinglot.service.ParkingLotService;
 
 public class ParkingLotTest {
+    private ParkingLotService parkingLotService;
+    @Before
+    public void setUp() throws Exception {
+        parkingLotService = new ParkingLotService();
+    }
+
     @Test
     public void givenCarRegistrationNumber_WhenParked_ShouldReturnTrue() {
         String carNumber = "UP12 AN3456";
-        ParkingLotService parkingLotService = new ParkingLotService();
         parkingLotService.parkTheCar(carNumber);
         boolean status = parkingLotService.isCarPresent(carNumber);
         Assert.assertTrue(status);
+    }
+
+    @Test
+    public void givenCarRegistrationNumber_WhenUnParked_ShouldReturnFalse() {
+        String carNumber1 = "UP12 AN3456";
+        String carNumber2 = "UP34 AN5678";
+        parkingLotService.parkTheCar(carNumber1);
+        parkingLotService.parkTheCar(carNumber2);
+        parkingLotService.unParkTheCar(carNumber1);
+        boolean status = parkingLotService.isCarPresent(carNumber1);
+        Assert.assertFalse(status);
     }
 }
