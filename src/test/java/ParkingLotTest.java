87c0f0497de11ca0d21834d61e811ebf4da011b4
Srijan-18 <srijan.mishrasir@gmail.com>
2020-07-25 14:49:35 +0530

    REFACTORED: Use of IntStream to initialise values of Vehicles and colour of vehicle introduction.

--- src/test/java/ParkingLotTest.java
+++ src/test/java/ParkingLotTest.java
@@ -28,7 +28,8 @@ public class ParkingLotTest {
 
     @Test
     public void givenAVehicle_WhenParked_ShouldReturnTrue() {
-        Vehicle vehicle = new Vehicle(Vehicle.DriverCategory.NORMAL, Vehicle.VehicleCategory.NORMAL);
+        Vehicle vehicle = new Vehicle(Vehicle.DriverCategory.NORMAL, Vehicle.VehicleCategory.NORMAL,
+                Vehicle.VehicleColour.NOT_SPECIFIED);
         parkingLotService.parkTheVehicle(vehicle);
         boolean status = parkingLotService.isVehiclePresent(vehicle);
         Assert.assertTrue(status);
@@ -36,7 +37,8 @@ public class ParkingLotTest {
 
     @Test
     public void givenAVehicleParked_WhenUnParked_ShouldReturnFalse() {
-        Vehicle vehicle = new Vehicle(Vehicle.DriverCategory.NORMAL, Vehicle.VehicleCategory.NORMAL);
+        Vehicle vehicle = new Vehicle(Vehicle.DriverCategory.NORMAL, Vehicle.VehicleCategory.NORMAL,
+                Vehicle.VehicleColour.NOT_SPECIFIED);
         parkingLotService.parkTheVehicle(vehicle);
         parkingLotService.unParkTheVehicle(vehicle);
         boolean status = parkingLotService.isVehiclePresent(vehicle);
@@ -45,9 +47,9 @@ public class ParkingLotTest {
 
     @Test
     public void givenParkingLotWithItsSize_WhenFullyOccupiedAndQueriedByOwner_ShouldReturnTrue() {
-        Vehicle[] vehicles = {new Vehicle(Vehicle.DriverCategory.NORMAL, Vehicle.VehicleCategory.NORMAL),
-                new Vehicle(Vehicle.DriverCategory.NORMAL, Vehicle.VehicleCategory.NORMAL),
-                new Vehicle(Vehicle.DriverCategory.NORMAL, Vehicle.VehicleCategory.NORMAL)};
+        Vehicle[] vehicles = new Vehicle[3];
+        IntStream.range(0, 3).forEachOrdered(index -> vehicles[index] = new Vehicle(Vehicle.DriverCategory.NORMAL,
+                Vehicle.VehicleCategory.NORMAL, Vehicle.VehicleColour.NOT_SPECIFIED));
         Arrays.stream(vehicles).forEachOrdered(vehicle -> parkingLotService.parkTheVehicle(vehicle));
         boolean status = owner.getParkingLotStatus();
         Assert.assertTrue(status);
@@ -56,10 +58,9 @@ public class ParkingLotTest {
     @Test
     public void givenVehiclesToPark_WhenAskedToParkBeyondSize_ShouldThrowAnException() {
         try {
-            Vehicle[] vehicles = {new Vehicle(Vehicle.DriverCategory.NORMAL, Vehicle.VehicleCategory.NORMAL),
-                    new Vehicle(Vehicle.DriverCategory.NORMAL, Vehicle.VehicleCategory.NORMAL),
-                    new Vehicle(Vehicle.DriverCategory.NORMAL, Vehicle.VehicleCategory.NORMAL),
-                    new Vehicle(Vehicle.DriverCategory.NORMAL, Vehicle.VehicleCategory.NORMAL)};
+            Vehicle[] vehicles = new Vehicle[4];
+            IntStream.range(0, 4).forEachOrdered(index -> vehicles[index] = new Vehicle(Vehicle.DriverCategory.NORMAL,
+                    Vehicle.VehicleCategory.NORMAL, Vehicle.VehicleColour.NOT_SPECIFIED));
             Arrays.stream(vehicles).forEachOrdered(vehicle -> parkingLotService.parkTheVehicle(vehicle));
         } catch (ParkingLotServiceException exception) {
             Assert.assertEquals(ParkingLotServiceException.ExceptionType.PARKING_FULL, exception.exceptionType);
@@ -69,7 +70,8 @@ public class ParkingLotTest {
     @Test
     public void givenVehicleToUnPark_WhenNotPresent_ShouldThrowAnException() {
         try {
-            parkingLotService.unParkTheVehicle(new Vehicle(Vehicle.DriverCategory.NORMAL, Vehicle.VehicleCategory.NORMAL));
+            parkingLotService.unParkTheVehicle(new Vehicle(Vehicle.DriverCategory.NORMAL,
+                    Vehicle.VehicleCategory.NORMAL, Vehicle.VehicleColour.NOT_SPECIFIED));
         } catch (ParkingLotServiceException exception) {
             Assert.assertEquals(ParkingLotServiceException.ExceptionType.VEHICLE_NOT_PRESENT, exception.exceptionType);
         }
@@ -77,9 +79,9 @@ public class ParkingLotTest {
 
     @Test
     public void givenParkingLot_WhenFullAndQueriedForFullParkingLotByAirportSecurity_ShouldReturnTrue() {
-        Vehicle[] vehicles = {new Vehicle(Vehicle.DriverCategory.NORMAL, Vehicle.VehicleCategory.NORMAL),
-                new Vehicle(Vehicle.DriverCategory.NORMAL, Vehicle.VehicleCategory.NORMAL),
-                new Vehicle(Vehicle.DriverCategory.NORMAL, Vehicle.VehicleCategory.NORMAL)};
+        Vehicle[] vehicles = new Vehicle[3];
+        IntStream.range(0, 3).forEachOrdered(index -> vehicles[index] = new Vehicle(Vehicle.DriverCategory.NORMAL,
+                Vehicle.VehicleCategory.NORMAL, Vehicle.VehicleColour.NOT_SPECIFIED));
         Arrays.stream(vehicles).forEachOrdered(vehicle -> parkingLotService.parkTheVehicle(vehicle));
         boolean status = airportSecurity.getParkingLotStatus();
         Assert.assertTrue(status);
@@ -87,8 +89,9 @@ public class ParkingLotTest {
 
     @Test
     public void givenParkingLot_WhenParkingLotNotFullAndCheckedByOwner_ShouldReturnFalse() {
-        Vehicle[] vehicles = {new Vehicle(Vehicle.DriverCategory.NORMAL, Vehicle.VehicleCategory.NORMAL),
-                new Vehicle(Vehicle.DriverCategory.NORMAL, Vehicle.VehicleCategory.NORMAL)};
+        Vehicle[] vehicles = new Vehicle[2];
+        IntStream.range(0, 2).forEachOrdered(index -> vehicles[index] = new Vehicle(Vehicle.DriverCategory.NORMAL,
+                Vehicle.VehicleCategory.NORMAL, Vehicle.VehicleColour.NOT_SPECIFIED));
         Arrays.stream(vehicles).forEachOrdered(vehicle -> parkingLotService.parkTheVehicle(vehicle));
         boolean status = owner.getParkingLotStatus();
         Assert.assertFalse(status);
@@ -96,9 +99,9 @@ public class ParkingLotTest {
 
     @Test
     public void givenAParkedVehicle_WhenQueriedForSlotNumber_ShouldReturnSlotNumber() {
-        Vehicle[] vehicles = {new Vehicle(Vehicle.DriverCategory.NORMAL, Vehicle.VehicleCategory.NORMAL),
-                new Vehicle(Vehicle.DriverCategory.NORMAL, Vehicle.VehicleCategory.NORMAL),
-                new Vehicle(Vehicle.DriverCategory.NORMAL, Vehicle.VehicleCategory.NORMAL)};
+        Vehicle[] vehicles = new Vehicle[3];
+        IntStream.range(0, 3).forEachOrdered(index -> vehicles[index] = new Vehicle(Vehicle.DriverCategory.NORMAL,
+                Vehicle.VehicleCategory.NORMAL, Vehicle.VehicleColour.NOT_SPECIFIED));
         Arrays.stream(vehicles).forEachOrdered(vehicle -> parkingLotService.parkTheVehicle(vehicle));
         Assert.assertEquals("P:1 S:2", parkingLotService.getSlotOfParkedVehicle(vehicles[1]));
     }
@@ -106,7 +109,8 @@ public class ParkingLotTest {
     @Test
     public void givenAVehicleNotPresentInParkingLot_WhenQueriedForSlotNumber_ShouldThrowAnException() {
         try {
-            parkingLotService.getSlotOfParkedVehicle(new Vehicle(Vehicle.DriverCategory.NORMAL, Vehicle.VehicleCategory.NORMAL));
+            parkingLotService.getSlotOfParkedVehicle(new Vehicle(Vehicle.DriverCategory.NORMAL,
+                    Vehicle.VehicleCategory.NORMAL, Vehicle.VehicleColour.NOT_SPECIFIED));
         } catch (ParkingLotServiceException exception) {
             Assert.assertEquals(ParkingLotServiceException.ExceptionType.VEHICLE_NOT_PRESENT, exception.exceptionType);
         }
@@ -115,7 +119,8 @@ public class ParkingLotTest {
     @Test
     public void givenAVehicle_WhenAlreadyParked_ShouldThrowAnException() {
         try {
-            Vehicle vehicle = new Vehicle(Vehicle.DriverCategory.NORMAL, Vehicle.VehicleCategory.NORMAL);
+            Vehicle vehicle = new Vehicle(Vehicle.DriverCategory.NORMAL, Vehicle.VehicleCategory.NORMAL,
+                    Vehicle.VehicleColour.NOT_SPECIFIED);
             parkingLotService.parkTheVehicle(vehicle);
             parkingLotService.parkTheVehicle(vehicle);
         } catch (ParkingLotServiceException exception) {
@@ -126,7 +131,8 @@ public class ParkingLotTest {
 
     @Test
     public void givenVehicleToPark_WhenParkedAndQueriedForTimeOfParking_ShouldReturnCurrentTime() {
-        Vehicle vehicle = new Vehicle(Vehicle.DriverCategory.NORMAL, Vehicle.VehicleCategory.NORMAL);
+        Vehicle vehicle = new Vehicle(Vehicle.DriverCategory.NORMAL, Vehicle.VehicleCategory.NORMAL,
+                Vehicle.VehicleColour.NOT_SPECIFIED);
         parkingLotService.parkTheVehicle(vehicle);
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm");
         LocalDateTime now = LocalDateTime.now();
@@ -136,7 +142,8 @@ public class ParkingLotTest {
     @Test
     public void givenVehicleNotPresentInParkingLot_WhenQueriedForTime_ShouldThrowAnException() {
         try {
-            parkingLotService.getTimeOfParkingForVehicle(new Vehicle(Vehicle.DriverCategory.NORMAL, Vehicle.VehicleCategory.NORMAL));
+            parkingLotService.getTimeOfParkingForVehicle(new Vehicle(Vehicle.DriverCategory.NORMAL,
+                    Vehicle.VehicleCategory.NORMAL, Vehicle.VehicleColour.NOT_SPECIFIED));
         } catch (ParkingLotServiceException exception) {
             Assert.assertEquals(ParkingLotServiceException.ExceptionType.VEHICLE_NOT_PRESENT, exception.exceptionType);
         }
@@ -145,10 +152,9 @@ public class ParkingLotTest {
     @Test
     public void givenMultipleParkingLotsAndVehicles_WhenParked_ShouldDistributeThemEvenly() {
         ParkingLotService lotService = new ParkingLotService(3, 3);
-        Vehicle[] vehicles = {new Vehicle(Vehicle.DriverCategory.NORMAL, Vehicle.VehicleCategory.NORMAL),
-                new Vehicle(Vehicle.DriverCategory.NORMAL, Vehicle.VehicleCategory.NORMAL),
-                new Vehicle(Vehicle.DriverCategory.NORMAL, Vehicle.VehicleCategory.NORMAL),
-                new Vehicle(Vehicle.DriverCategory.NORMAL, Vehicle.VehicleCategory.NORMAL)};
+        Vehicle[] vehicles = new Vehicle[4];
+        IntStream.range(0, 4).forEachOrdered(index -> vehicles[index] = new Vehicle(Vehicle.DriverCategory.NORMAL,
+                Vehicle.VehicleCategory.NORMAL, Vehicle.VehicleColour.NOT_SPECIFIED));
         Arrays.stream(vehicles).forEachOrdered(lotService::parkTheVehicle);
         Assert.assertEquals("P:1 S:2", lotService.getSlotOfParkedVehicle(vehicles[3]));
     }
@@ -157,10 +163,9 @@ public class ParkingLotTest {
     public void givenAVehicle_WhenAlreadyParkedInAnyOfLots_ShouldThrowAnException() {
         try {
             ParkingLotService parkingLotService = new ParkingLotService(3, 3);
-            Vehicle[] vehicles = {new Vehicle(Vehicle.DriverCategory.NORMAL, Vehicle.VehicleCategory.NORMAL),
-                    new Vehicle(Vehicle.DriverCategory.NORMAL, Vehicle.VehicleCategory.NORMAL),
-                    new Vehicle(Vehicle.DriverCategory.NORMAL, Vehicle.VehicleCategory.NORMAL),
-                    new Vehicle(Vehicle.DriverCategory.NORMAL, Vehicle.VehicleCategory.NORMAL)};
+            Vehicle[] vehicles = new Vehicle[4];
+            IntStream.range(0, 4).forEachOrdered(index -> vehicles[index] = new Vehicle(Vehicle.DriverCategory.NORMAL,
+                    Vehicle.VehicleCategory.NORMAL, Vehicle.VehicleColour.NOT_SPECIFIED));
             Arrays.stream(vehicles).forEachOrdered(parkingLotService::parkTheVehicle);
             parkingLotService.parkTheVehicle(vehicles[1]);
         } catch (ParkingLotServiceException exception) {
@@ -173,11 +178,9 @@ public class ParkingLotTest {
     public void givenVehiclesToParkInMultipleLots_WhenExceedSize_ShouldThrowAnException() {
         try {
             ParkingLotService parkingLotService = new ParkingLotService(2, 2);
-            Vehicle[] vehicles = {new Vehicle(Vehicle.DriverCategory.NORMAL, Vehicle.VehicleCategory.NORMAL),
-                    new Vehicle(Vehicle.DriverCategory.NORMAL, Vehicle.VehicleCategory.NORMAL),
-                    new Vehicle(Vehicle.DriverCategory.NORMAL, Vehicle.VehicleCategory.NORMAL),
-                    new Vehicle(Vehicle.DriverCategory.NORMAL, Vehicle.VehicleCategory.NORMAL),
-                    new Vehicle(Vehicle.DriverCategory.NORMAL, Vehicle.VehicleCategory.NORMAL)};
+            Vehicle[] vehicles = new Vehicle[5];
+            IntStream.range(0, 5).forEachOrdered(index -> vehicles[index] = new Vehicle(Vehicle.DriverCategory.NORMAL,
+                    Vehicle.VehicleCategory.NORMAL, Vehicle.VehicleColour.NOT_SPECIFIED));
             Arrays.stream(vehicles).forEachOrdered(parkingLotService::parkTheVehicle);
         } catch (ParkingLotServiceException exception) {
             Assert.assertEquals(ParkingLotServiceException.ExceptionType.PARKING_FULL, exception.exceptionType);
@@ -187,10 +190,14 @@ public class ParkingLotTest {
     @Test
     public void givenHandicappedDriver_WhenToPark_ShouldBeGivenNearestSlot() {
         ParkingLotService parkingLotService = new ParkingLotService(3, 3);
-        Vehicle[] vehicles = {new Vehicle(Vehicle.DriverCategory.NORMAL, Vehicle.VehicleCategory.NORMAL),
-                new Vehicle(Vehicle.DriverCategory.NORMAL, Vehicle.VehicleCategory.NORMAL),
-                new Vehicle(Vehicle.DriverCategory.HANDICAPPED, Vehicle.VehicleCategory.NORMAL),
-                new Vehicle(Vehicle.DriverCategory.NORMAL, Vehicle.VehicleCategory.NORMAL)};
+        Vehicle[] vehicles = {new Vehicle(Vehicle.DriverCategory.NORMAL, Vehicle.VehicleCategory.NORMAL,
+                Vehicle.VehicleColour.NOT_SPECIFIED),
+                new Vehicle(Vehicle.DriverCategory.NORMAL, Vehicle.VehicleCategory.NORMAL,
+                        Vehicle.VehicleColour.NOT_SPECIFIED),
+                new Vehicle(Vehicle.DriverCategory.HANDICAPPED, Vehicle.VehicleCategory.NORMAL,
+                        Vehicle.VehicleColour.NOT_SPECIFIED),
+                new Vehicle(Vehicle.DriverCategory.NORMAL, Vehicle.VehicleCategory.NORMAL,
+                        Vehicle.VehicleColour.NOT_SPECIFIED)};
         Arrays.stream(vehicles).forEachOrdered(parkingLotService::parkTheVehicle);
         Assert.assertEquals("P:1 S:2", parkingLotService.getSlotOfParkedVehicle(vehicles[2]));
     }
@@ -198,11 +205,11 @@ public class ParkingLotTest {
     @Test
     public void givenOnlyHandicappedDrivers_WhenToPark_ShouldParkSerially() {
         ParkingLotService parkingLotService = new ParkingLotService(2, 3);
-        Vehicle[] vehicles = {new Vehicle(Vehicle.DriverCategory.HANDICAPPED, Vehicle.VehicleCategory.NORMAL),
-                new Vehicle(Vehicle.DriverCategory.HANDICAPPED, Vehicle.VehicleCategory.NORMAL),
-                new Vehicle(Vehicle.DriverCategory.HANDICAPPED, Vehicle.VehicleCategory.NORMAL),
-                new Vehicle(Vehicle.DriverCategory.HANDICAPPED, Vehicle.VehicleCategory.NORMAL),
-                new Vehicle(Vehicle.DriverCategory.NORMAL, Vehicle.VehicleCategory.NORMAL)};
+        Vehicle[] vehicles = new Vehicle[5];
+        IntStream.range(0, 4).forEachOrdered(index -> vehicles[index] = new Vehicle(Vehicle.DriverCategory.HANDICAPPED,
+                Vehicle.VehicleCategory.NORMAL, Vehicle.VehicleColour.NOT_SPECIFIED));
+        vehicles [4] = new Vehicle(Vehicle.DriverCategory.NORMAL, Vehicle.VehicleCategory.NORMAL,
+                Vehicle.VehicleColour.NOT_SPECIFIED);
         Arrays.stream(vehicles).forEachOrdered(parkingLotService::parkTheVehicle);
         Assert.assertEquals("P:3 S:1", parkingLotService.getSlotOfParkedVehicle(vehicles[4]));
     }
@@ -212,7 +219,7 @@ public class ParkingLotTest {
         ParkingLotService parkingLotService = new ParkingLotService(2, 2);
         Vehicle[] vehicles = new Vehicle[3];
         IntStream.range(0, 3).forEachOrdered(index -> vehicles[index] = new Vehicle(Vehicle.DriverCategory.NORMAL,
-                                                                                     Vehicle.VehicleCategory.LARGE));
+                Vehicle.VehicleCategory.LARGE, Vehicle.VehicleColour.NOT_SPECIFIED));
         try {
             IntStream.range(0, 3).forEachOrdered(index -> parkingLotService.parkTheVehicle(vehicles[index]));
         } catch (ParkingLotServiceException exception) {
@@ -226,13 +233,13 @@ public class ParkingLotTest {
         ParkingLotService parkingLotService = new ParkingLotService(4, 3);
         Vehicle[] vehicles = new Vehicle[12];
         IntStream.range(0, 12).forEachOrdered(index -> vehicles[index] = new Vehicle(Vehicle.DriverCategory.NORMAL,
-                Vehicle.VehicleCategory.NORMAL ));
+                Vehicle.VehicleCategory.NORMAL, Vehicle.VehicleColour.NOT_SPECIFIED));
         IntStream.range(0, 12).forEachOrdered(index -> parkingLotService.parkTheVehicle(vehicles[index]));
         IntStream.rangeClosed(3, 5).forEachOrdered(index -> parkingLotService.unParkTheVehicle(vehicles[index]));
         IntStream.rangeClosed(7, 9).forEachOrdered(index -> parkingLotService.unParkTheVehicle(vehicles[index]));
         parkingLotService.unParkTheVehicle(vehicles[11]);
         Vehicle largeVehicleWithHandicappedDriver = new Vehicle(Vehicle.DriverCategory.NORMAL,
-                                                                Vehicle.VehicleCategory.LARGE);
+                Vehicle.VehicleCategory.LARGE, Vehicle.VehicleColour.NOT_SPECIFIED);
         parkingLotService.parkTheVehicle(largeVehicleWithHandicappedDriver);
         Assert.assertEquals("P:3 S:2", parkingLotService
                 .getSlotOfParkedVehicle(largeVehicleWithHandicappedDriver));
@@ -243,13 +250,13 @@ public class ParkingLotTest {
         ParkingLotService parkingLotService = new ParkingLotService(4, 3);
         Vehicle[] vehicles = new Vehicle[12];
         IntStream.range(0, 12).forEachOrdered(index -> vehicles[index] = new Vehicle(Vehicle.DriverCategory.NORMAL,
-                                                                                    Vehicle.VehicleCategory.NORMAL ));
+                Vehicle.VehicleCategory.NORMAL, Vehicle.VehicleColour.NOT_SPECIFIED));
         IntStream.range(0, 12).forEachOrdered(index -> parkingLotService.parkTheVehicle(vehicles[index]));
         IntStream.rangeClosed(3, 5).forEachOrdered(index -> parkingLotService.unParkTheVehicle(vehicles[index]));
         IntStream.rangeClosed(7, 9).forEachOrdered(index -> parkingLotService.unParkTheVehicle(vehicles[index]));
         parkingLotService.unParkTheVehicle(vehicles[11]);
         Vehicle largeVehicleWithHandicappedDriver = new Vehicle(Vehicle.DriverCategory.HANDICAPPED,
-                                                                Vehicle.VehicleCategory.LARGE);
+                Vehicle.VehicleCategory.LARGE, Vehicle.VehicleColour.NOT_SPECIFIED);
         parkingLotService.parkTheVehicle(largeVehicleWithHandicappedDriver);
         Assert.assertEquals("P:2 S:2", parkingLotService
                                                 .getSlotOfParkedVehicle(largeVehicleWithHandicappedDriver));
