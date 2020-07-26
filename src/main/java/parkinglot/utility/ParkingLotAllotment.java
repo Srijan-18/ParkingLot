24d97dc47f25454b68fec5cb3dabc5aeef44b7de
Srijan-18 <srijan.mishrasir@gmail.com>
2020-07-27 03:25:56 +0530

    REFACTORED: Correction made in use of comparator to sort the lots.

--- src/main/java/parkinglot/utility/ParkingLotAllotment.java
+++ src/main/java/parkinglot/utility/ParkingLotAllotment.java
@@ -3,6 +3,9 @@ package parkinglot.utility;
 import parkinglot.model.Vehicle;
 import parkinglot.service.ParkingLot;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class ParkingLotAllotment {
@@ -24,14 +27,9 @@ public class ParkingLotAllotment {
                         if (parkingLot.getNumberOfVehiclesParked() < this.parkingLotSize)
                             return parkingLot;
                     }
-                int minimumIndex = 0;
-                int minimumSize = parkingLots.get(0).getNumberOfVehiclesParked();
-                for (int index = 0; index < parkingLots.size(); index++)
-                    if (parkingLots.get(index).getNumberOfVehiclesParked() < minimumSize) {
-                        minimumIndex = index;
-                        minimumSize = parkingLots.get(index).getNumberOfVehiclesParked();
-                    }
-                availableParkingLot = parkingLots.get(minimumIndex);
+                List<ParkingLot> temporaryParkingLotsList = new ArrayList<>(parkingLots);
+                Collections.sort(temporaryParkingLotsList, Comparator.comparing(ParkingLot::getNumberOfVehiclesParked));
+                availableParkingLot = temporaryParkingLotsList.get(0);
         }
         return availableParkingLot;
     }
